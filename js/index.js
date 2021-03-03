536ffbad1a1d15b5722da65a0805775b0d54004c
yastrebvitali <yastrebvitali@gmail.com>
2021-03-03 16:51:09 +0300

    save

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var nam=5;
+console.log(nam);
+var suma=5+nam;
+var suma2;
+console.log(suma);
+console.log(suma2);
+var bar =null;
+console.log(bar);
+var bol=true;
+console.log(bol);
+
+/*let age = prompt('Сколько тебе лет?', 100);
+
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!*/
+console.log('------------------');
+console.log(bol);
+
